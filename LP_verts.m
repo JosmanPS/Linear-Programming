7d5e775c4e1da4f404ca3a4f3a6eb12028562a77
JosmanPS <jmps2812@gmail.com>
2014-08-31 20:39:29 -0500

    Improvement
    Improvement, quit one variable.

--- LP_verts.m
+++ LP_verts.m
@@ -19,10 +19,7 @@ if( size(b,2) > 1)
 end
 
 % Define the matrix verts.
-verts = zeros(l2,1);
-
-% A counter to indicate the column of verts
-ind = 1;
+verts = [];
 
 % Get the different square sub-matrix of A.
 for i = 1:(l2-2)
@@ -32,17 +29,19 @@ for i = 1:(l2-2)
             % Square sub-matrix of A.
             matrix = A(:, [i,j,k]);
            
+            x = zeros(5,1);
+            
             % Solve the system
-            x = inv(matrix) * b;
+            x([i,j,k], 1) = inv(matrix) * b;
            
             % If X >= 0, then X is a vertex.
             if(sum(x >= 0) == length(x))
                
                 % Write the vertex in the matrix.
-                verts([i,j,k], ind) = x;
-                ind = ind + 1;
+                verts = [verts, x];
                
             end       
         end
     end
 end
+
